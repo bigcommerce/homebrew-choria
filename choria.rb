b25a7d3854b0775adb6375e55cb12064223fff13
Joe Cai <joe.cai@bigcommerce.com>
2021-12-08 10:34:50 +1100

    Update choria.rb

--- choria.rb
+++ choria.rb
@@ -1,10 +1,10 @@
 class Choria < Formula
-  desc "A framework for building Control Planes, Orchestration Systems and Programmable Infrastructure"
+  desc "Orchestration System with roots in The Marionette Collective"
   homepage "https://choria.io/"
   url "https://github.com/choria-io/go-choria/archive/refs/tags/v0.24.1.tar.gz"
   sha256 "7d869b0b84b01582f4b1e1730fac32eb66b6bfe56726ef458370a0e72134ca88"
   license "Apache-2.0"
-  head "https://github.com/choria-io/go-choria.git"
+  head "https://github.com/choria-io/go-choria.git", branch: "main"
 
   livecheck do
     url :stable
