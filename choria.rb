4c197c077aaa5df3f3642ccb01afdab4f60563ea
Chris Boulton <chris@chrisboulton.com>
2024-07-18 09:07:30 -0700

    build choria 0.29.1

--- choria.rb
+++ choria.rb
@@ -1,8 +1,8 @@
 class Choria < Formula
   desc "Orchestration System with roots in The Marionette Collective"
   homepage "https://choria.io/"
-  url "https://github.com/choria-io/go-choria/archive/refs/tags/v0.27.0.tar.gz"
-  sha256 "e8725acf899414980b92175a48844aefea3001044ba6273d4aaea93ac2a896c5"
+  url "https://github.com/choria-io/go-choria/archive/refs/tags/v0.29.1.tar.gz"
+  sha256 "b2a5e8720e4ff6927d176f5d1c0eed394ce05c26ad174e680b5be2d4dcdc0fe8"
   license "Apache-2.0"
   head "https://github.com/choria-io/go-choria.git", branch: "main"
 
@@ -11,10 +11,10 @@ class Choria < Formula
     strategy :github_latest
   end
 
-  depends_on "go@1.20" => :build
+  depends_on "go@1.21" => :build
 
   def install
-    system Formula["go@1.20"].opt_prefix/"bin/go", "build", *std_go_args(ldflags: "-s -w"), "-o", bin/"choria", "main.go"
+    system Formula["go@1.21"].opt_prefix/"bin/go", "build", *std_go_args(ldflags: "-s -w"), "-o", bin/"choria", "main.go"
   end
 
   test do
